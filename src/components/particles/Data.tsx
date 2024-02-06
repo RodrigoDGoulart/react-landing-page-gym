4234eeb851da9a3c2f1249f92e82afe86aa5ca31
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-06 20:12:19 -0300

    first commit

---
+++ src/components/particles/Data.tsx
@@ -0,0 +1,212 @@
+export const NavLinks = [
+    {
+        name: "Início",
+        url: "/"
+    },
+    {
+        name: "Sobre Nós",
+        url: "/sobre"
+    },
+    {
+        name: "Agenda",
+        url: "/"
+    },
+    {
+        name: "Galeria",
+        url: "/"
+    },
+    {
+        name: "News",
+        url: "/news"
+    },
+    {
+        name: "Contacts",
+        url: "/contact"
+    },
+];
+
+
+
+export const HeroTexts = [
+    {
+        Heading: "Liberte sua Força Interior!",
+        Paragraph: "Bem-vindo ao ZenFit, onde o fitness encontra a diversão! Esteja pronto para liberar sua força interior e transformar sua vida!",
+        Button: "Junte-se Agora!"
+    },
+    {
+        Heading: "Novas Alturas de Fitness!",
+        Paragraph: "Supere seus limites e alcance a grandeza em nossa academia! Junte-se à nossa comunidade de apoio e embarque em uma jornada de autoaperfeiçoamento e empoderamento!",
+        Button: "Inscreva-se Hoje!"
+    },
+    {
+        Heading: "Fitness é Diversão Conosco!",
+        Paragraph: "Em nossa academia, fitness não é apenas uma rotina; é uma aventura agradável! Junte-se a nós nesta expedição fitness, onde diversão e resultados caminham juntos!",
+        Button: "Matricule-se Agora!"
+    }
+]
+
+
+export const AboutTexts = {
+    firstText: "Sobre o ZenFit",
+    secondText: "Bem-vindo",
+    caption: "Bem-vindo ao ZenFit, onde o fitness encontra a comunidade e a transformação acontece!",
+    paragraph1: "No ZenFit, somos mais do que apenas uma academia; somos uma família fitness apaixonada e solidária dedicada a ajudá-lo a alcançar seus objetivos de saúde e bem-estar. Nossa missão é criar um ambiente positivo e capacitador que o inspire a desafiar a si mesmo, abraçar um estilo de vida saudável e descobrir a melhor versão de si mesmo.",
+}
+
+
+export const OfferTexts = {
+    firstText: "O que fazemos",
+    secondText: "Toda a Nossa Oferta",
+    list: [
+        {
+            listCaption: "Programas para Perda de Peso",
+            text: "Nossa abordagem personalizada, orientação especializada e resultados comprovados o ajudarão a alcançar seus objetivos de fitness. Dê o primeiro passo em direção a um corpo e estilo de vida transformados hoje.",
+        },
+        {
+            listCaption: "Programas de Construção Muscular",
+            text: "Nossos treinadores especializados o guiarão através de treinos personalizados, ajudando-o a alcançar seus objetivos de construção muscular de forma eficiente e segura.",
+        },
+        {
+            listCaption: "Diferentes Aulas Especiais",
+            text: "Eleve seu treino a novas alturas com nossa Aula Especial intensa, onde você ultrapassará seus limites e alcançará resultados fitness notáveis.",
+        }
+    ],
+}
+
+
+export const MembershipPlans = {
+    firstText: "Tabelas de Preços",
+    secondText: "Planos de Associação",
+    cards: [
+        {
+            amount: 8,
+            duration: "dia",
+            caption: "Treino de Um Dia",
+            benefits: ["Acesso único a todas as academias", "Treinador em grupo", "Agende uma aula em grupo", "Orientação de fitness"]
+        },
+        {
+            amount: 49,
+            duration: "mês",
+            caption: "Associação de 12 Meses",
+            benefits: ["Aulas em grupo", "Discutir metas de fitness", "Treinador em grupo", "Orientação de fitness"]
+        },
+        {
+            amount: 65,
+            duration: "mês",
+            caption: "Pague Todo Mês",
+            benefits: ["Aulas em grupo", "Discutir metas de fitness", "Treinador em grupo", "Orientação de fitness"]
+        }
+    ]
+}
+
+
+export const ContactTexts = {
+    firstText: "ligue-nos hoje",
+    phone: "(11) 98765-4321",
+    paragraph: "Em nossa academia, você pode entrar em contato com qualquer um de nossos treinadores para agendar uma sessão. Serviços em domicílio também estão dentro do escopo de nossos serviços. Sua saúde é nossa preocupação! Saúde é riqueza!",
+    button: "Agende um Encontro"
+}
+
+
+export const TestimonialTexts = {
+    firstText: "nossos depoimentos",
+    secondText: "O que os Clientes Dizem",
+    feedBacks: [
+        {
+            text: "Sou membro há seis meses, e a comunidade de apoio da academia e os treinadores experientes me ajudaram a superar minhas metas de fitness!",
+            person: "Darrell Murray",
+            type: "Cliente"
+        },
+        {
+            text: "As aulas de Yoga transformaram minha vida; me sinto mais centrado, forte e tranquilo cada vez que piso no tapete.",
+            person: "Derrick Rodriquez",
+            type: "Cliente"
+        },
+        {
+            text: "Participar da Aula Especial foi um divisor de águas! É intenso, mas os resultados são incríveis. Altamente recomendado!",
+            person: "Collins Thompson",
+            type: "Cliente"
+        }
+    ],
+}
+
+
+export const BlogTexts = {
+    firstText: "Fique informado",
+    secondText: "Nossas Últimas Notícias",
+    blogNews: [
+        {
+            caption: "Yoga",
+            title: "Suas autorrealizações se desvanecem rapidamente?",
+            paragraph: "Descubra o poder transformador do yoga enquanto exploramos seus inúmeros benefícios físicos e mentais. Desde o aumento da flexibilidade e força até a redução do estresse e o aprimoramento da atenção plena, nossas aulas de yoga atendem a praticantes de todos os níveis. Junte-se a nós no tapete e experimente a harmonia da mente, corpo e alma.",
+            time: "1 min",
+            author: "Ralph Cruz",
+            comments: "21"
+        },
+        {
+            caption: "Treinadores e Equipamentos",
+            title: "Pequenas coisas fazem a diferença",
+            paragraph: "Em nossa academia, nos orgulhamos de nossa excepcional equipe de treinadores especializados que são apaixonados por guiá-lo em sua jornada fitness. Com seu conhecimento e abordagem personalizada, você receberá o suporte necessário para alcançar seus objetivos de fitness de forma eficaz. Além disso, oferecemos equipamentos de última geração projetados para elevar seus treinos e proporcionar uma experiência fitness holística como nenhuma outra.",
+            time: "15 min",
+            author: "Bruce Charles",
+            comments: "45"
+        },
+        {
+            caption: "Procrastinação",
+            title: "O Tempo é Agora",
+            paragraph: "A procrastinação pode ser um obstáculo comum no caminho para alcançar nossos objetivos. Neste blog, exploramos estratégias eficazes para superar a procrastinação e aumentar a produtividade. Desde técnicas de gerenciamento de tempo até manter a motivação, estamos aqui para ajudá-lo a assumir o controle de seus objetivos e abraçar uma vida mais focada e realizadora. É hora de aproveitar o dia e tornar seus sonhos realidade!",
+            time: "19 min",
+            author: "Steve Wagner",
+            comments: "55"
+        }
+    ]
+}
+
+
+export const FooterTexts = {
+    underLogoText: "Somos mais do que apenas uma academia; somos uma família fitness apaixonada e solidária dedicada a ajudá-lo a alcançar seus objetivos de saúde e bem-estar. Nossa missão é criar um ambiente positivo e capacitador que o inspire a desafiar a si mesmo, abraçar um estilo de vida saudável e descobrir a melhor versão de si mesmo.",
+    quickLinks: {
+        caption: "Links Rápidos",
+        links: [
+            {
+                name: "Home",
+                url: "/"
+            },
+            {
+                name: "About Us",
+                url: "/about"
+            },
+            {
+                name: "Schedule",
+                url: "/schedule"
+            },
+            {
+                name: "Gallery",
+                url: "/gallery"
+            },
+            {
+                name: "News",
+                url: "/news"
+            },
+            {
+                name: "Contacts",
+                url: "/contact"
+            },
+        ]
+    },
+    contacts: {
+        caption: "Contatos Rápidos",
+        names: [
+            {
+                name: "Rua Exemplo 1, Cidade Feliz, Estado da Alegria, 12345-678",
+            },
+            {
+                name: "contato@zenfit.com",
+            },
+            {
+                name: "+55 (11) 98765-4321",
+            }
+        ]
+    },
+    copyright: "Copyright 2024. All Right Reserved"
+}
