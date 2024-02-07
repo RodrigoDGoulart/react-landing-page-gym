ff2ba5b984191db529ae703c2e7efc5bf976b88a
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-02-07 17:31:03 -0300

    feat: insert contact banner

--- src/components/organs/NavBar.tsx
+++ src/components/organs/NavBar.tsx
@@ -1,45 +1,71 @@
-import { useState, useEffect } from "react"
-import { NavLinks } from "../particles/Data"
+import { useState, useEffect } from "react";
+import { NavLinks } from "../particles/Data";
 import { List } from "../atoms/List";
 import { NavLink } from "react-router-dom";
 import { ArrowCircleRight, Barbell } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 import { Text } from "../atoms/Text";
 
-
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false)
+  const [navBarColor, setNavBarColor] = useState(false);
 
-    const [navBarColor, setNavBarColor] = useState(false)
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
-    const handleToggle = () => {
-        setOpen(!open)
-    }
+  const listenScrollEvent = () => {
+    window.scrollY > 10 ? setNavBarColor(true) : setNavBarColor(false);
+  };
 
-    const listenScrollEvent = () => {
-        window.scrollY > 10 ? setNavBarColor(true) : setNavBarColor(false);
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
     };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", listenScrollEvent);
-        return () => {
-            window.removeEventListener("scroll", listenScrollEvent);
-        };
-    }, []);
-
-
-    return (
-        <header className="w-full h-auto bg-transparent overflow-x-hidden fixed z-50 top-0 left-0">
-            <nav className={`w-full lg:h-28 md:h-24 h-20 ${navBarColor ? "bg-zinc-900" : " bg-transparent"} lg:px-16 md:px-9 px-8 flex justify-between items-center`}>
-                <Link to={`/`} className="font-extrabold flex items-center relative md:text-2xl text-lg">
-                    <Text as="span" className="text-green-500 absolute -top-3 md:left-5 left-3">
-                        <Barbell size={25} color="currentColor" weight="fill" />
-                    </Text>
-                    <Text as="span" className="text-white">Zen</Text>
-                    <Text as="span" className="bg-gradient-to-r from-green-500 to-green-950 bg-clip-text text-transparent">Fit</Text>
-                </Link>
-                {/* <div className="lg:flex hidden items-center h-full gap-20">
+  return (
+    <header className="w-full h-auto bg-transparent overflow-x-hidden fixed z-50 top-0 left-0">
+      <div
+        style={{
+          color: "white",
+          backgroundColor: "#3bb100",
+          textAlign: "center",
+          fontSize: "1em",
+          paddingBlock: "8px",
+        }}
+      >
+        Gostou do modelo? Entre em contato conosco para fazer o seu! Contato:{" "}
+        {`(12) 99669-3634`}
+      </div>
+      <nav
+        className={`w-full lg:h-28 md:h-24 h-20 ${
+          navBarColor ? "bg-zinc-900" : " bg-transparent"
+        } lg:px-16 md:px-9 px-8 flex justify-between items-center`}
+      >
+        <Link
+          to={`/`}
+          className="font-extrabold flex items-center relative md:text-2xl text-lg"
+        >
+          <Text
+            as="span"
+            className="text-green-500 absolute -top-3 md:left-5 left-3"
+          >
+            <Barbell size={25} color="currentColor" weight="fill" />
+          </Text>
+          <Text as="span" className="text-white">
+            Zen
+          </Text>
+          <Text
+            as="span"
+            className="bg-gradient-to-r from-green-500 to-green-950 bg-clip-text text-transparent"
+          >
+            Fit
+          </Text>
+        </Link>
+        {/* <div className="lg:flex hidden items-center h-full gap-20">
                     <ul className="flex items-center justify-center h-full gap-4 relative before:w-full before:h-0.5 before:absolute before:bottom-0 before:left-0 before:bg-zinc-400">
                         {
                             NavLinks.map((navlink, index) => (
@@ -54,37 +80,54 @@ const NavBar = () => {
                 <div className="hamburger lg:hidden flex text-white cursor-pointer" onClick={handleToggle}>
                     <CirclesFour size={30} color="currentColor" weight="light" />
                 </div> */}
-            </nav>
+      </nav>
 
+      {/* Mobile Nav  */}
+      <nav
+        className={`flex justify-end lg:hidden h-screen w-full bg-gray-950/90 fixed top-0  ${
+          open ? "right-0" : "-right-[120vw]"
+        } transition-all duration-500 ease-out`}
+      >
+        <div
+          className={`w-full md:w-[50%] h-screen bg-zinc-900 flex flex-col justify-between items-center relative ${
+            open ? "right-0" : "-right-[120vw]"
+          } transition-all duration-500 ease-out delay-300`}
+        >
+          <section className="w-full px-4 py-6 flex flex-col gap-16">
+            <div className="w-full flex pt-5 px-4 justify-between items-center">
+              <Link to={`/`} className="font-extrabold text-2xl">
+                <span className=" text-white ">Zen</span>
+                <span className=" text-green-500">Fit</span>
+              </Link>
+              <div
+                className="hamburger text-white cursor-pointer"
+                onClick={handleToggle}
+              >
+                <ArrowCircleRight
+                  size={25}
+                  color="currentColor"
+                  weight="light"
+                />
+              </div>
+            </div>
+            <ul className="flex flex-col gap-3 pl-5">
+              {NavLinks.map((navlink, index) => (
+                <List className="w-full text-base" key={index}>
+                  <NavLink
+                    to={navlink.url}
+                    onClick={handleToggle}
+                    className={`relative overflow-hidden inline-block text-white before:w-full before:h-0.5 before:bg-color2 before:absolute before:bottom-0 before:-left-full before:rounded-full before:transition-all before:duration-200 before:ease-in hover:before:left-0 `}
+                  >
+                    {navlink.name}
+                  </NavLink>
+                </List>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-
-            {/* Mobile Nav  */}
-            <nav className={`flex justify-end lg:hidden h-screen w-full bg-gray-950/90 fixed top-0  ${open ? "right-0" : "-right-[120vw]"} transition-all duration-500 ease-out`}>
-                <div className={`w-full md:w-[50%] h-screen bg-zinc-900 flex flex-col justify-between items-center relative ${open ? "right-0" : "-right-[120vw]"} transition-all duration-500 ease-out delay-300`}>
-                    <section className="w-full px-4 py-6 flex flex-col gap-16">
-                        <div className="w-full flex pt-5 px-4 justify-between items-center">
-                            <Link to={`/`} className="font-extrabold text-2xl">
-                                <span className=" text-white ">Zen</span>
-                                <span className=" text-green-500">Fit</span>
-                            </Link>
-                            <div className="hamburger text-white cursor-pointer" onClick={handleToggle}>
-                                <ArrowCircleRight size={25} color="currentColor" weight="light" />
-                            </div>
-                        </div>
-                        <ul className="flex flex-col gap-3 pl-5">
-                            {
-                                NavLinks.map((navlink, index) => (
-                                    <List className="w-full text-base" key={index}>
-                                        <NavLink to={navlink.url} onClick={handleToggle} className={`relative overflow-hidden inline-block text-white before:w-full before:h-0.5 before:bg-color2 before:absolute before:bottom-0 before:-left-full before:rounded-full before:transition-all before:duration-200 before:ease-in hover:before:left-0 `}>{navlink.name}</NavLink>
-                                    </List>
-                                ))
-                            }
-                        </ul>
-                    </section>
-                </div>
-            </nav>
-        </header >
-    )
-}
-
-export default NavBar
+export default NavBar;
